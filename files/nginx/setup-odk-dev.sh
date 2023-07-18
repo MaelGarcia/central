c781aea06437534b80bb1e3e79b35421ad2c5432
MaelGarcia <edwinch.gar@gmail.com>
2023-07-18 04:40:56 -0600

    add setup config

--- files/nginx/setup-odk-dev.sh
+++ files/nginx/setup-odk-dev.sh
@@ -5,17 +5,13 @@ if [ "$SSL_TYPE" != "upstream" ] && [ ! -s "$DH_PATH" ]; then
   openssl dhparam -out "$DH_PATH" 2048
 fi
 
-SELFSIGN_PATH="/etc/selfsign/live/$DOMAIN"
-
 echo "writing fresh nginx templates..."
 cp /usr/share/odk/nginx/redirector.conf /etc/nginx/conf.d/redirector.conf
-CNAME=$( [ "$SSL_TYPE" = "customssl" ] && echo "local" || echo "$DOMAIN") \
-nvsubst '$SSL_TYPE $CNAME $SENTRY_ORG_SUBDOMAIN $SENTRY_KEY $SENTRY_PROJECT' \
-< /usr/share/odk/nginx/odk.conf.template \
-> /etc/nginx/conf.d/odk.conf
+
 
 if [ "$SSL_TYPE" = "letsencrypt" ]; then
-  echo "starting nginx for letsencrypt..."
+  echo "starting nginx for letsencrypt mode developt..."
+  echo "by EdwinGarcia"
   /bin/bash /scripts/start_nginx_certbot.sh
   exec nginx -g "daemon off;"
 fi

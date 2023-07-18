fd47c1e950f0aff50039979d24c96d98fae63aa3
MaelGarcia <edwinch.gar@gmail.com>
2023-07-18 03:09:06 -0600

    setup

--- files/nginx/setup-odk-dev.sh
+++ files/nginx/setup-odk-dev.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-
-
-
     DH_PATH=/etc/dh/nginx.pem
 
     SELFSIGN_PATH="/etc/selfsign/live/$DOMAIN"
+
     echo "writing fresh nginx templates..."
     cp /usr/share/odk/nginx/redirector.conf /etc/nginx/conf.d/redirector.conf
     if [ "$SSL_TYPE" = "letsencrypt" ]; then
-    echo "starting nginx for letsencrypt..."
+    echo "starting nginx for letsencrypt para desarrolladores..."
     /bin/bash /scripts/start_nginx_certbot.sh
+    exec nginx -g "daemon off;"
     fi
   
-    exec nginx -g "daemon off;"
+    
 

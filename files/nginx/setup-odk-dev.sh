208994885f72e33a7e41f44c238975c245ade4ce
MaelGarcia <edwinch.gar@gmail.com>
2023-07-18 03:42:17 -0600

    change dev

--- files/nginx/setup-odk-dev.sh
+++ files/nginx/setup-odk-dev.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-    DH_PATH=/etc/dh/nginx.pem
+DH_PATH=/etc/dh/nginx.pem
 
-    SELFSIGN_PATH="/etc/selfsign/live/$DOMAIN"
+SELFSIGN_PATH="/etc/selfsign/live/$DOMAIN"
 
-    echo "writing fresh nginx templates..."
-    cp /usr/share/odk/nginx/redirector.conf /etc/nginx/conf.d/redirector.conf
-    if [ "$SSL_TYPE" = "letsencrypt" ]; then
-    echo "starting nginx for letsencrypt para desarrolladores..."
-    /bin/bash /scripts/start_nginx_certbot.sh
-    exec nginx -g "daemon off;"
-    fi
+echo "writing fresh nginx templates..."
+cp /usr/share/odk/nginx/redirector.conf /etc/nginx/conf.d/redirector.conf
+CNAME=$( [ "$SSL_TYPE" = "customssl" ] && echo "local" || echo "$DOMAIN") \
+nvsubst '$SSL_TYPE $CNAME $SENTRY_ORG_SUBDOMAIN $SENTRY_KEY $SENTRY_PROJECT' \
+< /usr/share/odk/nginx/odk.conf.template \
+> /etc/nginx/conf.d/odk.conf
+
+if [ "$SSL_TYPE" = "letsencrypt" ]; then
+echo "starting nginx for letsencrypt para desarrolladores..."
+echo "dev EdwinGarcia"
+/bin/bash /scripts/start_nginx_certbot.sh
+fi
+exec nginx -g "daemon off;"
   
     
 
